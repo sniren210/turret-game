489fcf1b8250cdac772e9bb149edf6f22cff34d8
sniren210 <sniren2002@gmail.com>
2020-12-07 19:58:10 +0700

    commit pertama

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1607323901,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/enemy-sheet0.png",
+		"images/gridbackground.png",
+		"images/wall.png",
+		"images/enemytarget-sheet0.png",
+		"images/turret-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/enemyspawner-sheet0.png",
+		"images/bulletimpactparticles.png",
+		"images/enemydeadparticles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
